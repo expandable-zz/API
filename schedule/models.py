39dfff9e1c7150125ff7408e80ab4e00ac10e02d
unknown <Iso@Obsidian.(none)>
2011-11-04 11:37:03 +0100

    Ajout de modèles dans l’app « schedule »

--- schedule/models.py
+++ schedule/models.py
@@ -2,3 +2,22 @@
 from django.db import models
 
 # Create your models here.
+class EventCategory(models.Model):
+    label = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+    color = models.CharField(max_length=6, unique=True)
+
+class Event(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(EventCategory, related_name='category_set')
+    url = models.URLField(blank=True)
+    date_start = models.DateTimeField()
+    date_end = models.DateTimeField()
+    location = models.TextField(blank=True)
+    clubs = models.ManyToManyField('clubs.Club', related_name='clubs_set', blank=True)
+    private = models.BooleanField()
+    validated = models.NullBooleanField(default=None)
+    canceled = models.BooleanField(default=False)
+    price = models.IntegerField(default=0)
+    minors_allowed = models.BooleanField()
